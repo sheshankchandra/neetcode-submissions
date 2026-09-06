a8418cdb34484ade2d3142db01dc05aef5f9c318
Sheshank chandra <82082604+sheshankchandra@users.noreply.github.com>
2026-09-06 23:45:22 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        answerList = []
+        helperDict = {}
+
+        for l in range(len(strs)):
+            freq = [0] * 26
+            curStr = strs[l]
+
+            for i in range(len(curStr)):
+                freq[ord(curStr[i]) - ord('a')] += 1
+
+            dictKey = str(freq)
+            if dictKey in helperDict:
+                helperDict[dictKey].append(strs[l])
+                continue
+            helperDict[dictKey] = [strs[l]]
+
+        return list(helperDict.values())
